a03fdf510ce893857745bb42d8d9e54496fbcd5f
Souma sri <bojjasoumyasri991@gmail.com>
2026-05-19 22:20:20 +0530

    Fixed API URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-trl6.onrender.com",
+  baseURL: "https://team-task-manager-tr16.onrender.com",
 });
 
 export default API;
