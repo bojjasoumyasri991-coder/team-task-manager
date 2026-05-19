bff1dac5a0caa42efe28f7414c4faeb1610cced7
Souma sri <bojjasoumyasri991@gmail.com>
2026-05-19 21:48:03 +0530

    Auto create admin

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,26 @@ app.register_blueprint(tasks_bp)
 @app.route('/')
 def home():
     return {"message": "Team Task Manager API Running"}
+from werkzeug.security import generate_password_hash
+from models import User
+
+with app.app_context():
+    existing_admin = User.query.filter_by(
+        email="admin@gmail.com"
+    ).first()
+
+    if not existing_admin:
+        admin = User(
+            name="Admin",
+            email="admin@gmail.com",
+            password=generate_password_hash("admin123"),
+            role="admin"
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print("Admin created")
 
 if __name__ == '__main__':
     with app.app_context():
